a2f94b7d145cc35f7139f3fb3787a586c2bf6950
Gerard Gascón <ggasconmoline@gmail.com>
2025-10-15 17:40:43 +0200

    chore: convert repository into a upm-compatible project

---
+++ Assets/Samples/uTerminal/1.0.0/Cube Mover/SayHello.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * SayHello Class
+ * 
+ * This script defines a static class with a single command to demonstrate the usage of uTerminal.
+ * The command "hello" is associated with the method "Say," which logs "Hello, world!" to the uTerminal console.
+ *  
+ */
+
+namespace uTerminal.Samples
+{
+    public static class SayHello
+    {
+        // Command attribute marks the method as a uTerminal command with a specific name and description
+        [uCommand("hello", "say hello world example")]
+        public static void Say()
+        {
+            // Log a greeting message to the uTerminal console
+            uTerminalDebug.Log("Hello, world!");
+        }
+    }
+}
